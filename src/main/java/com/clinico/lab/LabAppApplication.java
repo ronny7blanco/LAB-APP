1ff8367271ec6498f1423b940c7d4b093bce1c30
ronny <ronnyblanco19@gmail.com>
2019-04-28 09:29:56 -0600

    primera version

---
+++ src/main/java/com/clinico/lab/LabAppApplication.java
@@ -0,0 +1,13 @@
+package com.clinico.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAppApplication.class, args);
+	}
+
+}
