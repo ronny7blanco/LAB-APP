46b0ef1fb1853e93ce3500e46382234678f7c09c
ronny7blanco <ronnyblanco19@gmail.com>
2019-05-15 16:33:03 -0600

    Entidades actualizadas

--- src/main/java/com/clinico/lab/models/entities/Usuarios.java
+++ src/main/java/com/clinico/lab/models/entities/Usuarios.java
@@ -13,19 +13,11 @@ import lombok.Setter;
 @Table(name="Usuarios")
 @Getter @Setter @NoArgsConstructor
 public class Usuarios {
+	
 	@Id 
-<<<<<<< HEAD
 	private Long idUsuario;
 	private String tipoEmpleado;
 	private String nombreUsuario;
 	private String contraseña;
 	private String estado;
-=======
-	private Long Id_Usuarios;
-	private String Nombre_de_Usuario;
-	private String Contraseña;
-	private String estado;
-	
->>>>>>> 15e6fedd8f40ef426e0c708c709d488633dd6986
-
 }
