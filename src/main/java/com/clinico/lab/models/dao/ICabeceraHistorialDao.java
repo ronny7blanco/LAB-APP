7035ec0e129d1a50fe0d01358d0d592eca621c06
wilbert1228 <waguilar341@gmail.com>
2019-05-26 11:56:05 -0600

    actualizacion de DAO

---
+++ src/main/java/com/clinico/lab/models/dao/ICabeceraHistorialDao.java
@@ -0,0 +1,10 @@
+package com.clinico.lab.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clinico.lab.models.entities.CabeceraHistorial;
+
+
+public interface ICabeceraHistorialDao extends JpaRepository<CabeceraHistorial , Long>{
+
+}
---
+++ src/main/java/com/clinico/lab/models/dao/IDetalleTipoExamenDao.java
@@ -0,0 +1,10 @@
+package com.clinico.lab.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clinico.lab.models.entities.DetalleTipoExamen;
+
+
+public interface IDetalleTipoExamenDao extends JpaRepository<DetalleTipoExamen , Long>{
+
+}
