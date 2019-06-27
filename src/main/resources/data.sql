2857c120b4be6a1f13e915766bd514b7150dffb2
ronny7blanco <ronnyblanco19@gmail.com>
2019-06-27 14:54:33 -0600

    data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,25 +9,25 @@ insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, 
   values ('03','Homero','Simpson','13/12/1996','homersimp48@yahoo.es','7666-6666','activo','doctor');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL)
-  values ('04','Ervin','Romero','06/07/2000','erv456@gmail.com','7000-1122','soltero','paciente');
+  values ('04','Ervin','Romero','06/07/2000','erv456@gmail.com','7000-1122','inactivo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL)
-  values ('05','Alex','Bonilla','02/05/1990/','albo12@gmail.com','7343-2312','casado','paciente');
+  values ('05','Alex','Bonilla','02/05/1990/','albo12@gmail.com','7343-2312','activo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL)
-  values ('06','Napo','Guevara','16/06/1997','nagu@gmail.com','7980-9878','soltero','paciente');
+  values ('06','Napo','Guevara','16/06/1997','nagu@gmail.com','7980-9878','activo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL )
-  values ('07','Nestor','Argueta','23/01/1995','near23@gmail.com','7565-6565','casado','paciente');
+  values ('07','Nestor','Argueta','23/01/1995','near23@gmail.com','7565-6565','inactivo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL)
-  values ('08','Josue','Ayala','12/11/1989','joay45@gmail.com','7555-6565','casado','paciente');
+  values ('08','Josue','Ayala','12/11/1989','joay45@gmail.com','7555-6565','activo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL)
-  values ('09','Kelly','Hernandez','28/04/1998','kelly28@gmail.com','7677-7778','casado','paciente');
+  values ('09','Kelly','Hernandez','28/04/1998','kelly28@gmail.com','7677-7778','activo','paciente');
   
 insert into "RONNY"."PERSONAS" (ID_PERSONA, NOMBRE, APELLIDO, FECHA_NACIMIENTO, E_MAIL, TELEFONO_CELULAR, ESTADO, ROL )
-  values ('10','Maria','Andrade','07/06/1997','maan12@gmail.com','7665-4321','casado','paciente');
+  values ('10','Maria','Andrade','07/06/1997','maan12@gmail.com','7665-4321','inactivo','paciente');
   
 -- TIPOS DE EXAMENES
 
